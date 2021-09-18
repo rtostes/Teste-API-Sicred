d1c38a8f9b42c7cf75b541767edb6c44635ce296
rodrigo.tostes <rodrigo.tostes@squadra.com.br>
2021-09-18 17:44:45 -0300

    TesteApiSicred

--- src/test/java/apiteste/restricoes.java
+++ src/test/java/apiteste/restricoes.java
@@ -15,11 +15,37 @@ public class restricoes {
 	
 	
 	@Test
-	public void consultaRestricoes() {
+	public void consultaRestricoesCPF19626829001() {
 		RestAssured.given()
 			.log().all()
 		.when()
 			.get("/api/v1/restricoes/19626829001")
+		.then()
+		.statusCode(200)
+			.log().all()
+		;
+		
+	}
+	
+	@Test
+	public void consultaRestricoesCPF60094146012() {
+		RestAssured.given()
+			.log().all()
+		.when()
+			.get("/api/v1/restricoes/60094146012")
+		.then()
+		.statusCode(200)
+			.log().all()
+		;
+		
+	}
+	
+	@Test
+	public void consultaRestricoesCPF84809766080() {
+		RestAssured.given()
+			.log().all()
+		.when()
+			.get("/api/v1/restricoes/84809766080")
 		.then()
 		.statusCode(200)
 			.log().all()
